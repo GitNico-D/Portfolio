104ff58e57e589769f64265641dacdffd04e20f0
GitNico-D <niicolas.dubief@gmail.com>
2020-10-16 17:10:06 +0200

    Adding createProject on projectController and modifying HTTP response code

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -24,7 +26,7 @@ class ProjectController extends AbstractController
         $data = $serializer->serialize($projects, 'json');
 
         $response = new Response($data);
-        $response->setStatusCode(200);
+        $response->setStatusCode(Response::HTTP_OK);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
@@ -41,8 +43,33 @@ class ProjectController extends AbstractController
         $data = $serializer->serialize($project, 'json');
 
         $response = new Response($data);
-        $response->setStatusCode(200);
+        $response->setStatusCode(Response::HTTP_OK);
         $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/projects", name="create_project", methods={"POST"})
+     */
+    public function createProject(
+        Request $request, 
+        SerializerInterface $serializer, 
+        UrlGeneratorInterface $urlGenerator
+        ): Response
+    {
+        $project = $serializer->deserialize($request->getContent(), Project::class, 'json');
+        
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $entityManager->persist($project);
+        $entityManager->flush();
+
+        $response = new Response(
+            $serializer->serialize($project, 'json'), 
+            Response::HTTP_CREATED,
+            ["Location" => $urlGenerator->generate("get_project", ["id" => $project->getId()])]
+        );
 
         return $response;
     }
