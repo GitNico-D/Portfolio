562385a39e7a27d55b5b04176475cd6bf4d50e95
Nico <niicolas.dubief@gmai.com>
2020-10-16 11:27:55 +0200

    Removing comment's lines

--- src/Repository/SoftwareRepository.php
+++ src/Repository/SoftwareRepository.php
@@ -18,33 +18,4 @@ class SoftwareRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Software::class);
     }
-
-    // /**
-    //  * @return Software[] Returns an array of Software objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Software
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
