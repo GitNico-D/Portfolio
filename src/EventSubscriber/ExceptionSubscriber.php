87b52358160ea136c513ccc805a36662a74dbf8e
GitNico-D <niicolas.dubief@gmail.com>
2020-11-06 15:18:28 +0100

    Add an error exception when method are not allowed(PUT a resource)

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -6,13 +6,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
-        if ($exception instanceof NotFoundHttpException) {
+        if (($exception instanceof NotFoundHttpException) || ($exception instanceof MethodNotAllowedHttpException)){
             $error = [
                 'status' => $exception->getStatusCode(),
                 'message' => $exception->getMessage()
