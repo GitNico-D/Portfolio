9bec7960c2d5ec9816b800227a9f20e8d859dee5
GitNico-D <niicolas.dubief@gmail.com>
2020-11-10 17:52:41 +0100

    Ctach a new specific Exception to return a corresponding  JsonResponse

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -10,17 +10,27 @@ use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
+    /**
+     * Return JsonResponse depending of the Exception catch
+     * An error 404 for no Route found
+     * An error 400 for specific Exception like 'Json Syntax error'
+     * 
+     * @param Exception $event
+     */
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
+        // dd($exception);
         if (($exception instanceof NotFoundHttpException) || ($exception instanceof MethodNotAllowedHttpException)){
             $error = [
                 'status' => $exception->getStatusCode(),
                 'message' => $exception->getMessage()
             ];            
-        $response = new JsonResponse($error);
-        $event->setResponse($response);
+            $response = new JsonResponse($error, JsonResponse::HTTP_NOT_FOUND);
+        } else {
+            $response = new JsonResponse(['message' => $exception->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
         }
+        $event->setResponse($response);
     }
 
     public static function getSubscribedEvents()
