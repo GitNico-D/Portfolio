d413257f942b80355cf04c6356fb92510b4801d7
GitNico-D <niicolas.dubief@gmail.com>
2020-11-12 10:43:58 +0100

    Correction

--- migrations/Version20201112093621.php
+++ migrations/Version20201112093621.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201110174230 extends AbstractMigration
+final class Version20201112093621 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20201110174230 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE category ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE education ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE experience ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE project ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE skill ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE software ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE category ADD created_at DATETIME, ADD updated_at DATETIME');
+        $this->addSql('ALTER TABLE education ADD created_at DATETIME, ADD updated_at DATETIME');
+        $this->addSql('ALTER TABLE experience ADD created_at DATETIME, ADD updated_at DATETIME');
+        $this->addSql('ALTER TABLE project ADD created_at DATETIME, ADD updated_at DATETIME, CHANGE create_at creation_date DATE NOT NULL');
+        $this->addSql('ALTER TABLE skill ADD created_at DATETIME, ADD updated_at DATETIME');
+        $this->addSql('ALTER TABLE software ADD created_at DATETIME, ADD updated_at DATETIME');
     }
 
     public function down(Schema $schema) : void
@@ -33,8 +33,8 @@ final class Version20201110174230 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category DROP created_at, DROP updated_at');
         $this->addSql('ALTER TABLE education DROP created_at, DROP updated_at');
-        $this->addSql('ALTER TABLE experience DROP updated_at');
-        $this->addSql('ALTER TABLE project DROP updated_at');
+        $this->addSql('ALTER TABLE experience DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE project DROP created_at, DROP updated_at, CHANGE creation_date create_at DATE NOT NULL');
         $this->addSql('ALTER TABLE skill DROP created_at, DROP updated_at');
         $this->addSql('ALTER TABLE software DROP created_at, DROP updated_at');
     }
