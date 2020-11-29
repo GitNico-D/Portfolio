368aed37590263cd643925b0e2df46a9dccec32e
GitNico-D <niicolas.dubief@gmail.com>
2020-11-29 12:22:23 +0100

    Remove

--- src/Hateoas/Link.php
+++ src/Hateoas/Link.php
@@ -4,6 +4,31 @@ namespace App\Hateoas;
 
 class Link
 {
-    private $href;
-    private $action;
+    /**
+     * @var string
+     */
+    public $action;
+
+    /**
+     * @var string
+     */
+    public $route;
+
+    /**
+     */
+    public function __construct(string $action, string $href)
+    {
+        $this->rel = $action;
+        $this->href = $href;
+    }
+
+    public function getHref(): string
+    {
+        return $this->href;
+    }
+
+    public function getAction(): string
+    {
+        return $this->action;
+    }
 }
