43b214754cfb695271615dd42f2ee2332e4c0521
GitNico-D <niicolas.dubief@gmail.com>
2020-12-17 15:55:18 +0100

    Adding homePage route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Homepage from "../views/Homepage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Homepage",
+    component: Homepage
   },
   {
     path: "/about",
