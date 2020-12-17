def09fd916d77d8044e0a2af64cf6faba0b57ab6
GitNico-D <niicolas.dubief@gmail.com>
2020-12-17 15:54:06 +0100

    Create a ButtonLink component

---
+++ frontend/src/components/ButtonLink.vue
@@ -0,0 +1,19 @@
+<template>
+	<b-button size="md" variant="info">
+			{{ action }}
+	</b-button>
+</template>
+
+<script>
+export default {
+	name: "ButtonLink",
+	props: {
+		action: String,
+		url: String
+	}
+}
+</script>
+
+<style>
+
+</style>
