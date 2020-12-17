5c87d337e24e7d2711305e5a189cf6e899b0909d
GitNico-D <niicolas.dubief@gmail.com>
2020-12-17 15:54:58 +0100

    Add nav menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+	<div id="app">
+		<nav id="nav">
+			<router-link to="/">Homepage | </router-link>
+			<router-link to="/about">About</router-link>
+		</nav>
+	<router-view />
+	</div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+body {
+	background-image: linear-gradient(to right top, #6d327c, #485DA6, #00a1ba, #00BF98, #36C486);
+
 }
 
 #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
