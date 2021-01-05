1d338d6477f4c490ffe62a03ddb230da0c5f1218
GitNico-D <niicolas.dubief@gmail.com>
2021-01-05 17:06:22 +0100

    Import gsap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@/styles/scss/global.scss';
+import { gsap } from 'gsap';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue)
