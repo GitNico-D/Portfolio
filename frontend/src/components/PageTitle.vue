dca90e48c9b0f16e0e37aa21a12165a2137e3d1b
GitNico-D <niicolas.dubief@gmail.com>
2021-01-14 19:17:42 +0100

    Create a component PageTitle

---
+++ frontend/src/components/PageTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 :style="{'--text-color': textColor}">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+    name: "PageTitle",
+    props: {
+        title: String,
+        textColor: String
+    }
+}
+</script>
+
+<style lang="scss">
+h1 {
+    @include customFont;
+    @include text_shadow(2px, 3px, 2px, var(--text-color)!important);
+    color: $white;            
+}
+</style>
