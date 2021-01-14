50bf720b8fea091067910b5ddf34b61a63b33c5c
GitNico-D <niicolas.dubief@gmail.com>
2021-01-14 19:18:16 +0100

    Create a menuNavigation component

---
+++ frontend/src/components/MenuNavigation.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-nav :style="{ '--link-color': linkColor, '--link-color-active': actoveLink}">
+        <router-link to="/"></router-link>
+        <router-link to="/projects">Projets </router-link> |
+        <router-link to="/experiences">Expériences |</router-link> | 
+        <router-link to="/skills">Compétences </router-link> | 
+        <router-link to="/career">Carrières</router-link> | 
+    </b-nav>    
+</template>
+
+<script>
+export default {
+    name: "MenuNavigation",
+    props: {
+        linkColor: String,
+        activeLink: String
+    }
+}
+</script>
+
+<style lang="scss">
+.nav {
+	padding: 30px;
+}
+.nav a {
+	font-weight: bold;
+	color: var(--link-color);
+}
+.nav a.router-link-exact-active {
+	color: var(--link-color-active);
+}
+</style>
