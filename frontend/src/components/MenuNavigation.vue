0f72418ba35bca2e290e006861c0472fc9a4fee9
GitNico-D <niicolas.dubief@gmail.com>
2021-01-15 16:49:17 +0100

    Update

--- frontend/src/components/MenuNavigation.vue
+++ frontend/src/components/MenuNavigation.vue
@@ -1,10 +1,11 @@
 <template>
-    <b-nav :style="{ '--link-color': linkColor, '--link-color-active': actoveLink}">
+    <b-nav :style="{ '--link-color': linkColor, '--action-color-link': actionColorLink}">
         <router-link to="/"></router-link>
-        <router-link to="/projects">Projets </router-link> |
-        <router-link to="/experiences">Expériences |</router-link> | 
-        <router-link to="/skills">Compétences </router-link> | 
-        <router-link to="/career">Carrières</router-link> | 
+        <router-link to="/projects">Projets</router-link><span :style="{'--span-color': spanColor}"> | </span>
+        <router-link to="/experiences">Expériences</router-link><span :style="{'--span-color': spanColor}"> | </span> 
+        <router-link to="/skills">Compétences</router-link><span :style="{'--span-color': spanColor}"> | </span>
+        <router-link to="/career">Carrières</router-link><span :style="{'--span-color': spanColor}"> | </span> 
+        <div class="active-indicator"></div>
     </b-nav>    
 </template>
 
@@ -13,20 +14,64 @@ export default {
     name: "MenuNavigation",
     props: {
         linkColor: String,
-        activeLink: String
+        actionColorLink: String
     }
 }
 </script>
 
 <style lang="scss">
 .nav {
-	padding: 30px;
-}
-.nav a {
-	font-weight: bold;
-	color: var(--link-color);
-}
-.nav a.router-link-exact-active {
-	color: var(--link-color-active);
+    padding: 1rem;
+    margin-left: 1rem;
+    a {
+        position: relative;
+        font-family: 'Oswald', sans-serif;
+        font-size: 0.8rem;
+        line-height: 1.6rem;        
+        color: var(--link-color);
+        text-transform: uppercase;
+        transition: ease-out 0.2s;
+        &::after {
+            position: absolute;
+            content: " ";
+            top: 100%;
+            left: 0;
+            width: 100%;
+            height: 3px;
+            background: var(--action-color-link);
+            transform: scaleX(0);
+            transform-origin: right;
+            transition: transform 0.5s;
+        }
+        &:hover::after {
+            transform: scaleX(1);
+            transform-origin: left;
+        }
+        &:hover {
+            color: var(--action-color-link);
+        }
+    }
+    .router-link-exact-active {
+        color: var(--action-color-link);
+        &:hover {
+            color: $white;
+        }
+    }
+    span {
+        padding: 0 1rem 0 1rem;
+        color: var(--action-color-link);
+        font-weight: bold;
+    }
+    .indicator
+    {
+        position: absolute;
+        left: 5%;
+        bottom: 0;
+        width: 30px;
+        height: 3px;
+        background-color: #fff;
+        border-radius: 5px;
+        transition: 0.2s ease left;
+    }
 }
 </style>
