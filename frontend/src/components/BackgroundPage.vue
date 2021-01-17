da1bda6806d8c21099e905dc015aac3713a9f43a
GitNico-D <niicolas.dubief@gmail.com>
2021-01-17 16:42:23 +0100

    Custom background for all page

--- frontend/src/components/BackgroundPage.vue
+++ frontend/src/components/BackgroundPage.vue
@@ -1,47 +1,61 @@
 <template>
     <div class="text-box">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,64L1440,224L1440,320L0,320Z"></path></svg>
-        <!-- <div class=""></div> -->
         <div class="one">
             {{pageTitle}}
         </div>
         <div class="two">
             {{pageTitle}}
         </div>
-    </div>   
+    </div>
 </template>
 
 <script>
 export default {
     name: "BackgroundPage",
     props: {
-        pageTitle: String
+        pageTitle: String,
+        gradientFirstColor: String,
+        gradientSecondColor: String
     }
 }
 </script>
 
 <style lang="scss">
 .text-box {
+    background: linear-gradient(150deg, $light-purple 0%, $light-purple 50%, $purple 50%, $purple 100%);
     position: relative;
     perspective: 1000px;
     font-family: 'MontSerrat',  sans-serif;
-    // height: 100vh;
+    width: 100vw;
+    height: 100vh;
+    transform: translate3d(0, 0, 1rem);
     .one {
         position: absolute;
         text-transform: uppercase;
         overflow: hidden;
-        transform: translate3d(200px, -80px, -35px);
+        top: 5%;
         color: rgba(53, 53, 53, 0.8);
-        font-size: 26rem;
+        font-size: 20rem;
+        animation: 25s linear infinite translatingOne;
+        filter: blur(0.5px);
     }
     .two {
         position: absolute;
         text-transform: uppercase;
         overflow: hidden;
         top: 0;
-        transform: translate3d(-110rem, -5rem, -1rem);
         color: rgba(245,245,245,0.8);
         font-size: 55rem;
+        filter: blur(4px);
+        animation: 10s linear infinite translatingTwo;
+    }
+    @keyframes translatingTwo {
+        from { transform: translateX(-250rem); }
+        to   { transform: translateX(200rem); }
+    }
+    @keyframes translatingOne {
+        from { transform: translateX(-125rem); }
+        to   { transform: translateX(125rem); }
     }
 }
 </style>
