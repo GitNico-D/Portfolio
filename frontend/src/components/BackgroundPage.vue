c3c41edbf1558618ee0abbf2c31b02f59d84554f
GitNico-D <niicolas.dubief@gmail.com>
2021-01-22 17:50:19 +0100

    Add an animation on backgroundPage circle

--- frontend/src/components/BackgroundPage.vue
+++ frontend/src/components/BackgroundPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="circlebackground -one" :style="{'--circle-color': circleColor }"></div>
+        <div class="circlebackground" :style="{'--circle-color': circleColor }"></div>
     </div>
 </template>
 
@@ -17,15 +17,22 @@ export default {
 .circlebackground {
     position: absolute;
     border-radius: 50%;
-    top: 50%;
-    right: 50%;
-    transform: translate(50%, -50%);
-    &.-one {
-        background-color: var(--circle-color);
-        opacity: 1;
-        width: 50vw;
-        height: 50vw;
-        @include box_shadow(0px, 0px, 20px, $purple);
+    top: 55%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: var(--circle-color);
+    opacity: 1;
+    width: 40vw;
+    height: 40vw;
+    @include box_shadow(0px, 0px, 55px, $purple);
+    animation: scale-up-center 0.8s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+}
+@keyframes scale-up-center {
+    0% {
+    transform: translate(-50%, -50%) scale(0);
+    }
+    100% {
+    transform:translate(-50%, -50%) scale(1);
     }
 }
 </style>
