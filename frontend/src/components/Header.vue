471c8b799ffca078e3283a4205594ecc870e0cb9
GitNico-D <niicolas.dubief@gmail.com>
2021-01-22 20:40:17 +0100

    Header responsive

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
-    <b-nav :style="{ '--link-color': linkColor, '--action-color-link': actionColorLink}">
-        <router-link to="/"></router-link>
-        <router-link to="/projects">Projets</router-link><span :style="{'--span-color': spanColor}"> | </span>
-        <router-link to="/experiences">Expériences</router-link><span :style="{'--span-color': spanColor}"> | </span> 
-        <router-link to="/skills">Compétences</router-link><span :style="{'--span-color': spanColor}"> | </span>
-        <router-link to="/career">Carrières</router-link><span :style="{'--span-color': spanColor}"> | </span> 
-        <div class="active-indicator"></div>
-    </b-nav>    
+    <b-row>
+        <b-navbar toggleable="sm">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav :style="{ '--link-color': linkColor, '--action-color-link': actionColorLink}">
+                    <router-link to="/projects">Projets</router-link><span :style="{'--span-color': spanColor}"> | </span>
+                    <router-link to="/experiences">Expériences</router-link><span :style="{'--span-color': spanColor}"> | </span> 
+                    <router-link to="/skills">Compétences</router-link><span :style="{'--span-color': spanColor}"> | </span>
+                    <router-link to="/career">Carrières</router-link><span :style="{'--span-color': spanColor}"> | </span> 
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <h1 :style="{'--text-color': textColor}" class="">{{ title }}</h1>
+    </b-row>
 </template>
 
 <script>
@@ -22,59 +28,133 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.nav {
-    padding: 1rem;
-    margin-left: 1rem;
-    a {
-        position: relative;
-        font-family: 'Oswald', sans-serif;
-        font-size: 1rem;
-        line-height: 1.6rem;        
-        color: var(--link-color);
-        text-transform: uppercase;
-        transition: ease-out 0.2s;
-        &::after {
-            position: absolute;
-            content: " ";
-            top: 100%;
-            left: 0;
-            width: 100%;
-            height: 3px;
-            background: var(--action-color-link);
-            transform: scaleX(0);
-            transform-origin: right;
-            transition: transform 0.5s;
+<style lang="scss" scoped>
+.row {
+    height: 20vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    .navbar {
+        width: 100%;
+        &-nav {
+            padding: 1rem;
+            margin: auto;
+            text-align: center;
+            a {
+                position: relative;
+                font-family: 'Oswald', sans-serif;
+                color: var(--link-color);
+                text-transform: uppercase;
+                transition: ease-out 0.2s;
+                &::after {
+                    position: absolute;
+                    content: " ";
+                    top: 100%;
+                    left: 0;
+                    width: 100%;
+                    height: 3px;
+                    background: var(--action-color-link);
+                    transform: scaleX(0);
+                    transform-origin: right;
+                    transition: transform 0.5s;
+                }
+                &:hover::after {
+                    transform: scaleX(1);
+                    transform-origin: left;
+                }
+                &:hover {
+                    color: var(--action-color-link);
+                }
+            }
+            .router-link-exact-active {
+                color: var(--action-color-link);
+                &:hover {
+                    color: $white;
+                }
+            }
+            span {
+                padding: 0 1rem 0 1rem;
+                color: var(--action-color-link);
+                font-weight: bold;
+            }
+            .indicator
+            {
+                position: absolute;
+                left: 5%;
+                bottom: 0;
+                width: 30px;
+                height: 3px;
+                background-color: #fff;
+                border-radius: 5px;
+                transition: 0.2s ease left;
+            }
+            &-collapse {
+                flex-direction: column;
+            }
         }
-        &:hover::after {
-            transform: scaleX(1);
-            transform-origin: left;
-        }
-        &:hover {
-            color: var(--action-color-link);
-        }
-    }
-    .router-link-exact-active {
-        color: var(--action-color-link);
-        &:hover {
+        button {
             color: $white;
+            background-color: $green!important;
+            border: 1px solid var(--text-color)!important;
         }
     }
-    span {
-        padding: 0 1rem 0 1rem;
-        color: var(--action-color-link);
-        font-weight: bold;
-    }
-    .indicator
-    {
-        position: absolute;
-        left: 5%;
-        bottom: 0;
-        width: 30px;
-        height: 3px;
-        background-color: #fff;
-        border-radius: 5px;
-        transition: 0.2s ease left;
+    h1 {
+    @include customFont;
+    text-shadow: 0 1px 0 var(--text-color),
+        0 2px 0 var(--text-color), 
+        0 3px 0 var(--text-color), 
+        0 4px 0 var(--text-color), 
+        0 5px 0 var(--text-color),
+        0 6px 0 var(--text-color),
+        0 7px 0 var(--text-color), 
+        0 8px 0 var(--text-color), 
+        0 9px 0 var(--text-color),
+        0 50px 30px rgba(109, 50, 124, 0.3);
+    color: $white;            
+    text-transform: uppercase;
     }
 }
+@media (min-width: 320px) {
+    .row {
+        height: 25vh;
+        h1 {
+            font-size: 1.8rem!important;            
+        }
+        .navbar {
+            button {
+                margin: auto!important;
+                margin-top: 1rem!important;
+            } 
+            &-nav {
+                a {
+                    margin-bottom: 0.5rem;
+                }
+                span {
+                    display: none;
+                }
+            }   
+        } 
+    }
+}
+@media (min-width: 576px) {
+    .row {
+        h1 {
+            font-size: 2.2rem!important;
+        }
+        .navbar {
+            &-nav {
+                span {
+                    display: block;
+                }
+            }   
+        }  
+    }
+}
+@media (min-width: 576px) {
+    .row {
+        h1 {
+            font-size: 2.8rem!important;
+        }
+    }
+} 
 </style>
