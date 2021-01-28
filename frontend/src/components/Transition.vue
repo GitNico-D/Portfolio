82bdf450f57cab8a2be66ab91200d3f56ee076da
GitNico-D <niicolas.dubief@gmail.com>
2021-01-28 17:40:12 +0100

    Add all transition direction

--- frontend/src/components/Transition.vue
+++ frontend/src/components/Transition.vue
@@ -1,8 +1,8 @@
 <template>
     <div :class="directionAnimation">
-    <div class="slider-green" >
-        <div class="slider-purple"></div>
-    </div>
+        <div class="slider-green" >
+            <div class="slider-purple"></div>
+        </div>
     </div>
 </template>
 
@@ -11,100 +11,150 @@ export default {
     name: "Transition",
     props: {
         directionAnimation: String
-    },
-    data() {
-        return {
-            hide: true
-        }
-    },
-    mounted() {
-        setTimeout(() => {
-            this.hide = false;
-        },1500);
-    },
+    }
 }
 </script>
 
 <style lang="scss">
+.slider {
+    &-green {
+        background: $green;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 255;
+    }
+    &-purple {
+        background: $purple;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        z-index: 255;
+    }        
+}
 .up {
-    .slider {
-        &-green {
-            background: $green;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            transform: translateY(0px);
-            animation: ease-in-out 1.5s slideUpGreen;
-            z-index: 255;
+    .slider-green {
+        animation: ease-in-out 1.5s slideUpGreen;
+    }
+    .slider-purple {
+        animation: ease-in-out 1.5s slideUpPurple;
+        transition-delay: 0.5s;
+    }
+    @keyframes slideUpGreen { 
+        from { 
+            transform: translateY(0%);
+            filter: blur(5px);
         }
-        &-purple {
-            background: $purple;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            transform: translateY(100px);
-            animation: ease-in-out 1.5s slideUpPurple;
-            transition-delay: 0.5s;
-            z-index: 255;
+        to  { 
+            transform: translateY(-110%);
+            filter: blur(0px);
         }
-        @keyframes slideUpGreen { 
-            from { 
-                transform: translateY(0%);
+    }       
+    @keyframes slideUpPurple { 
+        from { 
+            transform: translateY(100%);
+            height: 100%;
+            filter: blur(10px);
             }
-            to  { 
-                transform: translateY(-110%);
-            }
-        }       
-        @keyframes slideUpPurple { 
-            from { 
-                transform: translateY(100%);
-                height: 100%;
-                }
-            to  { 
-                transform: translateY(-110%);
-                height: 0%;
-            }
+        to  { 
+            transform: translateY(-110%);
+            height: 0%;
+            filter: blur(0px);
         }
     }
 }
-.up {
+.right {
     .slider {
         &-green {
-            transform: translateY(0px);
-            animation: ease-in-out 1.5s slideRightGreen;
+            animation: ease-in-out 1.3s slideRightGreen;
         }
         &-purple {
-            animation: ease-in-out 1.5s slideRightPurple;
+            animation: ease-in-out 1.4s slideRightPurple;
             transition-delay: 0.5s;
         }
         @keyframes slideRightGreen { 
             from { 
-                transform: translateY(0%);
+                transform: translateX(0%);
                 filter: blur(5px);
             }
             to  { 
-                transform: translateY(-110%);
+                transform: translateX(-110%);
                 filter: blur(0);
             }
         }       
         @keyframes slideRightPurple { 
             from { 
-                transform: translateY(100%);
-                height: 100%;
-                filter: blur(5px);
+                transform: translateX(100%);
+                filter: blur(10px);
                 }
             to  { 
-                transform: translateY(-110%);
-                height: 0%;
+                transform: translateX(-110%);
                 filter: blur(0);
             }
+        }
+    }
+}
+.left {
+    .slider {
+        &-green {
+            animation: ease-in-out 1.5s slideLeftGreen;
+        }
+        &-purple {
+            animation: ease-in-out 1.5s slideLeftPurple;
+            transition-delay: 0.5s;
+        }
+        @keyframes slideLeftGreen { 
+            from { 
+                transform: translateX(-110%);
+                filter: blur(5px);
+            }
+            to  { 
+                transform: translateX(110%);
+                filter: blur(0);
+            }
+        }       
+        @keyframes slideLeftPurple { 
+            from { 
+                transform: translateX(-110%);
+                filter: blur(10px);
+                }
+            to  { 
+                transform: translateX(100%);
+                filter: blur(0);
+            }
+        }
+    }
+}
+.down {
+    .slider-green {
+        animation: ease-in-out 1.5s slideDownGreen;
+    }
+    .slider-purple {
+        animation: ease-in-out 1.3s slideDownPurple;
+        transition-delay: 0.5s;
+    }
+    @keyframes slideDownGreen { 
+        from { 
+            transform: translateY(0%);
+            filter: blur(5px);
+        }
+        to  { 
+            transform: translateY(110%);
+            filter: blur(0px);
+        }
+    }       
+    @keyframes slideDownPurple { 
+        from { 
+            transform: translateY(-100%);
+            height: 100%;
+            filter: blur(10px);
+            }
+        to  { 
+            transform: translateY(110%);
+            height: 0%;
+            filter: blur(0px);
         }
     }
 }
