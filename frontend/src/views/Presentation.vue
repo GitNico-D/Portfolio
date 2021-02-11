b3428b82a0760d04364a6edf22d3334906e62df5
GitNico-D <niicolas.dubief@gmail.com>
2021-02-11 11:00:25 +0100

    Replace Experience view by Presentation view

---
+++ frontend/src/views/Presentation.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-container fluid>
+        <Header title="Projets" color="#6d327c"/>
+        <BackgroundPage circleColor="#6d327c"/>
+        <Transition v-show="showTransition" directionAnimation="left"/>
+        <b-row class="back">
+            <HomePageLink action="Retour" url="/" direction="animated-arrowLtr" class="link link-left" textColor="#6d327c"/>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
